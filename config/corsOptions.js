64b5c9f992beaecd8ddcef2dc8dd8830bd054ac6
MPiland <piland.mark@gmail.com>
2023-05-06 18:58:02 -0500

    Updated CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@ import allowedOrigins from './allowedOrigins.js'
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(req.header('Origin')) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
